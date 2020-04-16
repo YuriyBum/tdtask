7f8d20c3c97257f65f7fe5d97c5ced0d7b9e252e
YuriyBum <32727655+YuriyBum@users.noreply.github.com>
2020-04-16 13:17:05 +0500

    Update DigitComparator.java

--- src/tandemservicetask1/DigitComparator.java
+++ src/tandemservicetask1/DigitComparator.java
@@ -10,7 +10,7 @@ public class DigitComparator implements Comparator<ComparedList> {
 	public int compare(ComparedList o1, ComparedList o2) throws IndexOutOfBoundsException {
 		
 		StringAnalyzer an = new StringAnalyzer();
-		// TODO Auto-generated method stub
+		// Находим наибольшее число в каждой строке и по нему сравниваем
 		int r = 0;
 		if (o1.getComparedList().size() > 0) {
 			r = an.SelectLargestDigit(o1.getComparedList().get(o1.getColumnIndex())) 
