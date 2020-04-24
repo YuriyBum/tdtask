60daa3a727d670968c20037086cabc0a31811aec
YuriyBum <32727655+YuriyBum@users.noreply.github.com>
2020-04-24 13:12:46 +0500

    Подправлены комментарии

--- src/tandemservicetask1/tdstask1.java
+++ src/tandemservicetask1/tdstask1.java
@@ -7,14 +7,14 @@ import java.util.List;
 public class tdstask1 {
 	
 	
-	//Вспомогательный класс для проверки реализации с тестовыми входными данными
+	//<h3>Вспомогательный класс для проверки реализации с тестовыми входными данными</h3>
 	
 	public static void main(String[] args) {
 		
 			StringAnalyzer an = new StringAnalyzer();
 			
 		Task1Impl task = new Task1Impl();
-		//Тестовый входной надор данных
+		//<p>Тестовый входной надор данных</p>
 		List<String[]> testList = new ArrayList<String[]>();
 		
 		
@@ -32,7 +32,7 @@ public class tdstask1 {
 			case 3: {String[] e = {"Location con" , "Sheduled 568 units", "jrg 5 ok 32", "Attached 13 units", "","", "xcom 5 ok", "givan ok", null, null, null, null, null};
 			testList.add(e);}
 			break;
-			case 4: {String[] e = {"" , "Шоколадка 100г 438 ккал", "Барбарис 200г 800ккал", "Шоколадка 50г шоколадная 5г шоколад 50г начинка", "Водка",null, "12137", "600000", 
+			case 4: {String[] e = {"" , "Шоколадка 100г 438 ккал", "Барбарис 200г 800ккал", "Шоколадка 50г шоколадная 5г шоколад 50г начинка", "Водка",null, "12137", "60000", 
 					"8 Мой дядя самый честный 4652 года выпуска плавал 34 раза 7 раз тонул 12 раз спасся. 6152 дня бегал 7583 дня летал", 
 					"8 Мой папа честный 8137 года выпуска плавал 52 раза 9 раз тонул 11 раз спасся. 1723 дня бегал 92 дня летал",
 					"8 Мой брат честный 9188 года выпуска плавал 67 раза 56 раз тонул 5 раз спасся. 7445 дня бегал 92 дня летал",
@@ -56,7 +56,7 @@ public class tdstask1 {
 		
 		task.sort(testList, 4);
 		
-	//Проверяем геттер
+	//<p>Проверяем геттер</p>
 		try {
 		 for (int i = 0; i<task.getSortedRows().size(); i++) { for (int j = 0;
 		 j<task.getSortedRows().get(i).length; j++) {
@@ -67,7 +67,7 @@ public class tdstask1 {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-	//роверка разделителя	 
+	//<p>Проверка разделителя</p>	 
 		 
 		 System.out.println(an.SplittedString("8 Мой дядя самый честный 4652 года выпуска плавал 34 раза 7 раз тонул 12 раз спасся. 6152 дня бегал 7583 дня летал"));
 		 System.out.println(an.FinallySplittedString(an.SplittedString("8 Я же честный 8137 года выпуска плавал 52 раза 9 раз тонул 11 раз спасся. 1482 дня бегал 92 дня летал")));
